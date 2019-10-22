7ab15e9e1e6a16c1286ac63f3aef499d33c039d8
sunset <1451795113@qq.com>
2019-10-22 17:17:59 +0800

    文件上传的实现

--- src/main/java/com/sunset/controller/DynamicController.java
+++ src/main/java/com/sunset/controller/DynamicController.java
@@ -1,8 +1,11 @@
 package com.sunset.controller;
 
 import com.sunset.Mentity.Result;
+import com.sunset.Mentity.StateCode;
 import com.sunset.model.TDynamic;
 import com.sunset.service.DynamicService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,14 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
  * @author sunset
  * @date 2019-10-22 13:08
  */
+@Api(description = "朋友圈动态")
 @RestController
 @RequestMapping("/Dynamic")
 public class DynamicController {
     @Autowired
     private DynamicService dynamicService;
-    @RequestMapping(value = "/addDynamic",method = RequestMethod.POST)
+    @ApiOperation(value = "发布动态")
+    @RequestMapping(value = "/addDynamic",method = RequestMethod.POST, headers = "content-type=multipart/form-data")
     public Result addDynamic(@RequestBody TDynamic dynamic){
-        return null;
+        try {
+            dynamicService.addDynamic(dynamic);
+            return new Result(true,"发布成功", StateCode.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false,"发布失败",StateCode.ERROR);
+        }
 
     }
 }
---
+++ src/main/java/com/sunset/controller/UploadController.java
@@ -0,0 +1,40 @@
+package com.sunset.controller;
+
+import com.sunset.Mentity.Result;
+import com.sunset.Mentity.StateCode;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+@Api(description = "文件上传")
+@RestController
+@RequestMapping("/upload")
+public class UploadController {
+    @ApiOperation("上传图片")
+    @RequestMapping(value = "/uploadPhoto",method = RequestMethod.POST,headers = "content-type=multipart/form-data")
+    public Result uploadPhoto(MultipartFile file) {
+        try {
+            String filePath = "E:\\upload";
+            //获取文件拓展名
+            String filename = file.getOriginalFilename();
+            //新的文件按名
+            String newFileName = UUID.randomUUID()+filename;
+            File targetFile = new File(filePath, newFileName);
+            file.transferTo(targetFile);
+            String reality = filePath+"\\"+newFileName;
+            return new Result(true,"上传成功", StateCode.OK,reality);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new Result(true,"上传失败", StateCode.OK);
+        }
+
+    }
+}
